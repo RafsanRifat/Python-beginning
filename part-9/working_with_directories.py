85d19d2577432cefd32d03c92a6932d98d336302
Md. Rafsanjani <rafsan.rifat255@gmail.com>
2021-04-28 12:59:24 +0600

    working with directories completed

--- part-9/working_with_directories.py
+++ part-9/working_with_directories.py
@@ -1,15 +1,35 @@
-def is_leap(year):
-    leap = False
-
-    if year % 4 == 0 and (year % 400 == 0 or year % 100 != 0):
-        leap = True
-    else:
-        leap = False
-
-
-    return leap
+from pathlib import Path
+path = Path("C:\\rafsan\\practice\\python\\Mosh-paid-course\\part-9")
+# print(path.exists())     ---> the path exist or not
+# path.mkdir()          ---> to creat directories
+# path.rmdir()            --->  to remove directories
+# path.rename()           ---> to rename a directories
 
 
-year = int(input())
-print(is_leap(year))
+print(path.iterdir())          # with this method, we can get the files and directories in this path.  eita amaderke
+                               # ekta generator object return korbe.
 
+for p in path.iterdir():        # if we loop over it, it will return all the files and directories in this path.
+                                # ei kaj tai amra list comprehension use kore korte pari.
+    print(p)
+
+
+paths = [p for p in path.iterdir()]
+print(paths)                            # eikhane amra list akare kichu WindowsPath object pabo. pathlib module er Path
+                                        # class er 2ta base class holo ---> WindowsPath r PosixPath,   PosixPath holo
+                                        # unix,linux,mac er jonno. r WindowsPath holo windows er jonne... ... ...
+
+paths = [p for p in path.iterdir() if p.is_file()]   # eikhane extra ekta condition use kora holo---> j sudhu file gulo,
+                                                     # bamer p te return korbe
+print(paths)
+
+# path.iterdir()  function ta useful, kintu eitar limitation ache. eita, recursively search and pattern search korte parena.
+# eikharone amader glob() method use korte hoy.
+
+glob_all_file_search = [p for p in path.glob("*.*")]        # "*.*" eita dara all file bujhay
+print(glob_all_file_search)
+glob_py_file_search = [p for p in path.glob("*.py")]        # "*.py" eita dara joto .py file ache segulo ke bujhacche
+print(glob_py_file_search)
+
+rglob_py_file_search = [p for p in path.rglob("*.py")]        # rglob() use kora hoy recursively search korar jonno.
+print(rglob_py_file_search)
---
+++ part-9/working_with_files.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+path = Path("practice.py")
+print(path.exists())
