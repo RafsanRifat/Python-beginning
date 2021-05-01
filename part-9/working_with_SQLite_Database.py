340aafead8b92ecacd014558afbf9bb0c5d31f5b
Md. Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-01 13:47:59 +0600

    Working with SQLite database is completed

--- part-9/working_with_SQLite_Database.py
+++ part-9/working_with_SQLite_Database.py
@@ -12,4 +12,19 @@ movies = json.loads(Path("movies.json").read_text())   # movies.json file k sudh
                    # close korte hoy.   so, eikaj with diyea korai valo.
 
 with sqlite3.connect("db.sqlite3") as connection:
-    
+    command = "INSERT OR IGNORE INTO Movies VALUES(?, ?, ?)"      # our movie table will have 3 columns
+    for movie in movies:
+        connection.execute(command, tuple(movie.values()))
+    connection.commit()             # All those changes will be returned to the database with this
+
+
+# how to read data from our existing database ========>>>>>>
+
+with sqlite3.connect("db.sqlite3") as connection:  # to read the data from the database, we need to connect to the databse.
+    command_to_read_data = "SELECT * FROM Movies"  # Here we have selected all the data of the table-->"Movies"
+    cursor = connection.execute(command_to_read_data)  # this will return us a cursor type object. that is Iterable.
+    # for single_data in cursor:   # eita active rakhle nicher way ta kaj korbena. coz, after we iterate of the cursor,
+                                   # we will get to the end of the cursor
+    #     print(single_data)
+    table_data_list = cursor.fetchall()  # we will get all the movie list of the table from this way as an/a array/list
+    print(table_data_list)
