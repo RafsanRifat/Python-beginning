b2ca7683be7f54dd8d053907a0b73db3d7d4bc2b
Md. Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-04 11:34:34 +0600

    pip-hsohksnns completed, or 2 num tutorial completed

---
+++ part-10/pip-hsohsksnns.py
@@ -0,0 +1,28 @@
+# installation of some important pip packages invironment.
+
+
+# pip install requests   ====>>>  for sending http requests.
+# pip list    ===>>> for showing the packages that are currently installed in my machine.
+# pip install requests==2.9.2    ====>>> eivabe kono specifique version install kora jay.
+# pip install requests==2.9.*   ====>>>  eivabe kono version er  compatible version install hobe.
+# pip install request~=2.9.0    ====>>> eivabe 2.9.0 er latest compatible version install hobe.
+# pip uninstall requests       ====>>> eivabe kono package uninstall hobe, seita jei version er e hok.
+# pip install requests==2*     ====>>> this will install the  latest compatible version of version 2
+
+
+
+# R EI INSTALL KORA KONO PACKAGE KE AMRA PYTHON ER STANDARD LIBRARY ER MODULE ER MOTO KOREI USE KORTE PARBO.
+
+
+
+
+
+
+# =====>>> Semantic version =====>>>
+
+# Semantic Versioning is a versioning scheme for using meaningful version numbers (that's why it is called Semantic Versioning)
+#  Semantic Versioning works by structuring each version identifier into three parts, MAJOR , MINOR , and PATCH
+  # jodi amar computer er request package er Semantic version dekhi, tahole --  "requests   2.25.1" . eikhane,-->
+  # 2 holo Major part, that means --> Breaking changes
+  # 25 holo Minor part, that means Backwards-compatible changes
+  # 1 holo Patch, that means bug fixes.
