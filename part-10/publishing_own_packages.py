5a2a8d908b73da6f4b13c2eaa836a2c9be9f52b5
Md. Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-06 13:10:29 +0600

    publishing packages completed

---
+++ part-10/publishing_own_packages.py
@@ -0,0 +1,15 @@
+# To publish our Own package in  pypi.org,  we need to follow the following steps =======>>>
+
+
+"""
+    1. have to create an account in pypi.org
+    2. globally we have to install 3 tools ====>>
+                a) pip install setuptools
+                b) pip install wheel
+                c) pip install twine
+    3. Then follow, mosh video  steps and complete those.
+    4. now, for finally upload that package to pypi.org ------>>>
+            twine upload dist/*     --> run this command then it will ask for pypi.org user name and password. provide those,
+            and thae package will upload.
+
+"""
