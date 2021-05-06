6bf969dcf65838f0d68486575e803ecca69bafc3
Md. Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-06 10:54:36 +0600

    virtual invironment completed

---
+++ part-10/virtual_environment.py
@@ -0,0 +1,19 @@
+# protita project er jonno alada alada kore virtual invironment toiri kora hoy. oi project er chahida onujayee
+# package gulo use korar jonno...
+
+# virtual invironment create korar jonno pipenv install korte gobe.
+
+# tarpor virtual invironment a kono package install korar jonno ------>>>
+
+    # pipenv package-name   -- command use korte hobe.
+
+# package virtual invironment a package kaj koranor jonno, virtual invironment active korte hobe. Active korar jonno-->>
+        # pipenv shell    --command use korte hobe.
+
+# virtual invironment deactive korar jonno ------------->>>
+        #  exit   ---command use korte hobe.
+
+
+# ======>>>  pipenv install korte hobe globally, mane  pc er cmd te.   r tarpor pipenv er command gulo run korte hobe
+# editor er terminal a...
+
