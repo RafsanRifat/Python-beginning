e5f5ab9058d18dbd56c2fd7aa1ded12d236c211f
Md. Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-06 12:46:28 +0600

    Managing dependencies completed

---
+++ part-10/Managing_dependencies_of_application.py
@@ -0,0 +1,10 @@
+# Some useful commands of pipenv/managing directories ======>>>
+
+"""
+    pipenv graph    ====>>                  To see the list of all the installed dependencies.
+    pipenv uninstall package name ====>>    To uninstall a package
+    pipenv update --outdated      ====>>    To show the outdated packages
+    pipenv update package name    ====>>    To update a package
+    pipenv update                 ====>>    To update all the packages
+
+"""
